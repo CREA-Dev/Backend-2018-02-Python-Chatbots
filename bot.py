90238b37e80e28779d7806bad466e214bd310c53
Nathanaël Khodl <nkhodl@gmail.com>
2018-02-05 21:09:49 +0100

    add displaying of coordinates based on location

--- bot.py
+++ bot.py
@@ -5,17 +5,25 @@ Launch script with a command like
    python bot.py "123456:ThisIsYourChatBotToken"
 """
 
-import sys
-from telegram.ext import Updater, CommandHandler
+import sys, logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 def hello(bot, update):
    update.message.reply_text(
        'Hello {} !'.format(update.message.from_user.first_name)
    )
 
+def where_is(bot, update):
+    user_location = update.message.location
+    update.message.reply_text(
+        'Vous avez envoyé les coordonnées {};{}.'.format(user_location.latitude, user_location.longitude)
+    )
+
 updater = Updater(sys.argv[1]) # Avec notre token en argument
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello)) # On accepte la commande "Hello"
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(MessageHandler(Filters.location, where_is)) # On accepte la commande "Hello"
 
 updater.start_polling()
 updater.idle()
