31237c36d120ab46fb6a20b685ac43c0c158eae3
Nathanaël Khodl <nkhodl@gmail.com>
2018-02-07 19:11:59 +0100

    add logging and state changes

--- bot.py
+++ bot.py
@@ -5,14 +5,26 @@ Launch script with a command like
    python bot.py "123456:ThisIsYourChatBotToken"
 """
 
-import sys
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import sys, logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+
+
+# We put some identifiers in variables
+STATE_HELLO = "state hello"
+STATE_INTRO = "state intro"
 
 
 def hello(bot, update):
-   update.message.reply_text(
-       'Hello {} !'.format(update.message.from_user.first_name)
-   )
+    update.message.reply_text(
+        'Hello {} !'.format(update.message.from_user.first_name)
+    )
+    return STATE_INTRO  # Change state
+
+def show_intro(bot, update):
+    update.message.reply_text(
+        "Après ce message, on repasse en mode hello"
+    )
+    return STATE_HELLO # Change state
 
 def where_is(bot, update):
     user_location = update.message.location
@@ -20,10 +32,31 @@ def where_is(bot, update):
         'Vous avez envoyé les coordonnées {};{}.'.format(user_location.latitude, user_location.longitude)
     )
 
+
+# Showing debugging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+
+# Creation of a conversation handler
+conv_handler = ConversationHandler(
+        entry_points = [CommandHandler('hello', hello)],
+        states = {
+            STATE_HELLO: [
+                CommandHandler('hello', hello),
+                CommandHandler('location', where_is)
+            ],
+
+            STATE_INTRO: [
+                CommandHandler('hello', show_intro)
+            ],
+        },
+        fallbacks = []
+    )
+
 updater = Updater(sys.argv[1]) # Avec notre token en argument
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(MessageHandler(Filters.location, where_is)) # On accepte la commande "Hello"
+updater.dispatcher.add_handler(conv_handler)
 
 updater.start_polling()
 updater.idle()
